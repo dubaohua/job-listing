5db070f26965e8f2f652763888d037e317349b02
baohua <113983633@qq.com>
2017-01-10 00:51:24 +0800

    implement step 5 jiaven

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,6 +2,10 @@ class JobsController < ApplicationController
   before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def show
     @job = Job.find(params[:id])
+    if @job.is_hidden
+      flash[:warning] = "This Job already archieved"
+      redirect_to root_path
+    end
   end
   def index
     @jobs = Job.where(is_hidden: false).order('created_at DESC')
